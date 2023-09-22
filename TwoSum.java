8975c207e752bff4412a9a4d3e49b0f9a5fcc2db
Sagar Chhabriya <129516589+SagarChhabriya@users.noreply.github.com>
2023-09-22 18:12:34 +0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,10 +1,10 @@
 class TwoSum{
     public int[] twoSum(int[] nums, int target) {
-        
+        //Each element will be taken and compared to all other elements
         for(int i=0; i<nums.length; i++)
             for(int j=i+1; j<nums.length; j++)
                 if(nums[i]+nums[j]==target)
-                    return new int[]{i,j};
-   	 return new int[]{};
+                    return new int[]{i,j};        //if the sum of elements on indices i and j = target then return those indices
+   	 return new int[]{};  //otherwise return an new empty array
     }
 }
